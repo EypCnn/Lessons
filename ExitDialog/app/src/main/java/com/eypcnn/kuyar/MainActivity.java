0c2ea3848a446a94be6feecc87453c5bec01d48b
Eyüp Can Yılmaz <developereypcnn@gmail.com>
2017-12-05 23:02:57 +0300

    Exit Dialog Lesson by EypCnn
    Exit Dialog Lesson by EypCnn

---
+++ ExitDialog/app/src/main/java/com/eypcnn/kuyar/MainActivity.java
@@ -0,0 +1,38 @@
+package com.eypcnn.kuyar;
+
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onBackPressed(){
+        final AlertDialog.Builder builder= new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("Çıkış Yapmak İstiyor Musunuz?");
+        builder.setCancelable(true);
+        builder.setNegativeButton("Hayır", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+
+        builder.setPositiveButton("Evet", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                finish();
+            }
+        });
+
+        AlertDialog alertDialog= builder.create();
+        alertDialog.show();
+    }
+}
